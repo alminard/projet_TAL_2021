2d9aba92a0613aee73acb5c7d5691c7b458950af
anne-lyse <annelyseminard60@gmail.com>
2021-11-22 18:16:01 +0100

    add comments

--- evaluate_chunking.py
+++ evaluate_chunking.py
@@ -1,4 +1,8 @@
 #python evaluate_chunking.py file.conll
+#evaluation of automatic chunking (semoral tool)
+#file format : token \t ref \t sys
+#ref and sys follow the BILOU annotation format
+#seqeval : https://github.com/chakki-works/seqeval
 from seqeval.metrics import accuracy_score
 from seqeval.metrics import classification_report
 from seqeval.metrics import f1_score
@@ -7,15 +11,19 @@ import sys
 
 y_true = []
 y_pred = []
-f = open(sys.argv[1],"r")
 sent_t = []
 sent_p = []
+
+f = open(sys.argv[1],"r")
+
 for line in f :
     line = line.rstrip()
+
     if "\t" in line:
         elt = line.split("\t")
         t = elt[1]
         p = elt[2]
+        #Modification of the annotation format : from BILOU to IOBES
         t = t.replace("L-", "E-").replace("U-","S-")
         p = p.replace("L-", "E-").replace("U-","S-")
         sent_t.append(t)
@@ -27,9 +35,6 @@ for line in f :
         sent_t = []
         sent_p = []
 f.close()
-
-#print (y_true)
-#print (y_pred)
 
 print (f1_score(y_true, y_pred))
 
